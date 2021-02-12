c29fe262d66862b6e2d42f01c8a3cc9c5848fccf
Ali Emadi <ali7ev@gmail.com>
2021-02-12 19:51:52 +0330

    some changes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,15 +1,49 @@
-import React, { Component } from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import ProfileList from './ProfileList';
 
-export default class Profile extends Component{
-    
-    
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: 80
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
-    render(){
-        console.log('home render');
-        if (localStorage.getItem('token')){
+export default function Profile() {
+  const classes = useStyles();
 
-            return <h2>بروفایل کاربر</h2>
-        }
-        return <h2>لطفا وارد شوید.</h2>
-    }
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+              <ProfileList></ProfileList>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+              
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+              
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+              
+          </Paper>
+        </Grid>
+        
+      </Grid>
+    </div>
+  );
 }
